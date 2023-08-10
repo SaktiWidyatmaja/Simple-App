8a6e63eb98209dba09c2491bab8582fc994edc1c
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2023-08-10 14:14:19 +0700

    feat: add email to user model

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,12 +7,19 @@ const jwt = require('jsonwebtoken');
 // Register endpoint
 router.post('/register', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password, email } = req.body;
+
+    // Validate input
+    if (!username || !password || !email) {
+      return res.status(400).json({ message: 'Username, password, and email are required' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       username,
       password: hashedPassword,
+      email,
     });
 
     await user.save();
@@ -39,6 +46,24 @@ router.post('/login', async (req, res) => {
     } else {
       res.status(401).json({ message: 'Authentication failed' });
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete user endpoint
+router.delete('/delete/:username', async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    // Find and delete the user
+    const deletedUser = await User.findOneAndDelete({ username });
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
